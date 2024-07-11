d7b547e17424013f78ca37a19d5fd14dd7f3398d
syawalmuis <msyawalmuis@gmail.com>
2024-07-11 21:12:15 +0800

    fix: share game

--- app/api/share/[id]/route.ts
+++ app/api/share/[id]/route.ts
@@ -1,10 +1,11 @@
-import { FileToJSON } from "@/utils/converter";
+import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function GET(_: Request, context: any) {
     const { params } = context;
-    const game = FileToJSON(params.id);
+    const { rows } =
+        await sql`select * from games where id=${params.id} limit 1`;
 
-    if (!game) return NextResponse.json(game, { status: 404 });
-    return NextResponse.json(JSON.parse(String(game)));
+    if (rows.length <= 0) return NextResponse.json(false, { status: 404 });
+    return NextResponse.json(JSON.parse(String(rows[0].body)));
 }
--- app/api/share/route.ts
+++ app/api/share/route.ts
@@ -1,8 +1,20 @@
-import { FileToJSON, JSONToFile } from "@/utils/converter";
+import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+    // let game = {};
     const data = (await request.json()) as Game;
-    // JSONToFile(data, data.id);
+    const body = JSON.stringify(data);
+    const { rows } = await sql`select * from games where id=${data.id} limit 1`;
+    if (rows.length > 0) {
+        await sql`update games set body=${body} where id=${data.id}`;
+    } else {
+        await sql`insert into games (id, body) values (${data.id}, ${body})`;
+    }
     return NextResponse.json(data);
+}
+
+export async function GET(request: Request) {
+    const games = await sql`select * from games limit 1`;
+    return NextResponse.json(games.rows);
 }
