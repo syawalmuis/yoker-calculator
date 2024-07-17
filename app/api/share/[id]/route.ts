bd6c75fd36ff141c78100b51b204ec749ec66111
Muh Syawal Muis <82781937+syawalmuis@users.noreply.github.com>
2024-07-18 01:48:26 +0800

    Update route.ts

--- app/api/share/[id]/route.ts
+++ app/api/share/[id]/route.ts
@@ -4,8 +4,8 @@ import { NextResponse } from "next/server";
 export async function GET(_: Request, context: any) {
     const { params } = context;
     const { rows } =
-        await sql`select * from games where id=${params.id} limit 1`;
+        await sql`select * from games where id=${params.id}`;
 
     if (rows.length <= 0) return NextResponse.json(false, { status: 404 });
-    return NextResponse.json(JSON.parse(String(rows[0].body)));
+    return NextResponse.json(JSON.parse((rows)));
 }
