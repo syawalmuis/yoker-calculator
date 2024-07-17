3583b0a9a51abfe1b6cf572b4e80e4d3463b7ef8
Muh Syawal Muis <82781937+syawalmuis@users.noreply.github.com>
2024-07-18 02:31:28 +0800

    Update route.ts

--- app/api/share/[id]/route.ts
+++ app/api/share/[id]/route.ts
@@ -1,6 +1,8 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(_: Request, context: any) {
     const { params } = context;
     const data =
