4a548268837e9bcf1c34d80d56a6360f06407b53
Muh Syawal Muis <82781937+syawalmuis@users.noreply.github.com>
2024-08-03 20:53:04 +0800

    Update page.tsx

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -25,7 +25,7 @@ function Page({ params }: { params: { id: string } }) {
 
     return (
         <App>
-            <main className="px-5 py-10 min-h-[85vh]">
+            <main className="px-5 py-10 min-h-[90vh]">
                 <div className="card bg-base-100 md:max-w-lg w-full mx-auto mt-8">
                     <div className="card-body">
                         <h1 className="card-title mb-4 flex justify-between gap-5 items-center">
@@ -75,7 +75,7 @@ function Page({ params }: { params: { id: string } }) {
                 <div className="fixed bottom-0 w-full bg-base-100 left-0 z-20 pt-6 pb-6 flex items-center justify-center">
                     <Link
                         href={`/game/${params.id}/score`}
-                        className="border-white border-2 bg-primary h-14 w-14 flex items-center justify-center rounded-full text-white fill-white absolute -top-8 right-8"
+                        className="border-white border-2 bg-primary h-14 w-14 flex items-center justify-center rounded-full text-white fill-white absolute -top-8"
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
